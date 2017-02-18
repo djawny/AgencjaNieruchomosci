1b14afb53c601b5373f5d1cb573ffd8f825c8314
djawny <djawny@op.pl>
2017-02-18 15:11:09 +0100

    zm

---
+++ app/src/main/java/com/sdaacademy/jawny/daniel/agencjanieruchomosci/view/ProductDetailsActivity.java
@@ -0,0 +1,20 @@
+package com.sdaacademy.jawny.daniel.agencjanieruchomosci.view;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.sdaacademy.jawny.daniel.agencjanieruchomosci.R;
+
+import butterknife.ButterKnife;
+
+public class ProductDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_details);
+        ButterKnife.bind(this);
+
+    }
+
+}
