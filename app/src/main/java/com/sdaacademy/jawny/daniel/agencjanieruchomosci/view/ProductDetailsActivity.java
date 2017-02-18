0eaf2b66b537a86e36c3786172f095977fb076b8
djawny <djawny@op.pl>
2017-02-18 15:16:37 +0100

    zm

--- app/src/main/java/com/sdaacademy/jawny/daniel/agencjanieruchomosci/view/ProductDetailsActivity.java
+++ app/src/main/java/com/sdaacademy/jawny/daniel/agencjanieruchomosci/view/ProductDetailsActivity.java
@@ -2,12 +2,21 @@ package com.sdaacademy.jawny.daniel.agencjanieruchomosci.view;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
 
 import com.sdaacademy.jawny.daniel.agencjanieruchomosci.R;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class ProductDetailsActivity extends AppCompatActivity {
+
+    @BindView(R.id.product_image)
+    ImageView mProductImage;
+
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +24,10 @@ public class ProductDetailsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_product_details);
         ButterKnife.bind(this);
 
+
+
+//        setupToolbar();
+//        displayData();
     }
 
 }
