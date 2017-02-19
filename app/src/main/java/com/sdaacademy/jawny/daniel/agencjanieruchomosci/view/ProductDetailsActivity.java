823f268ec2a362b7b2497bcd7e40d371bb4ed403
djawny <djawny@op.pl>
2017-02-19 19:21:09 +0100

    zm

--- app/src/main/java/com/sdaacademy/jawny/daniel/agencjanieruchomosci/view/ProductDetailsActivity.java
+++ app/src/main/java/com/sdaacademy/jawny/daniel/agencjanieruchomosci/view/ProductDetailsActivity.java
@@ -1,9 +1,11 @@
 package com.sdaacademy.jawny.daniel.agencjanieruchomosci.view;
 
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 import android.widget.ImageView;
 
 import com.sdaacademy.jawny.daniel.agencjanieruchomosci.R;
@@ -24,6 +26,9 @@ public class ProductDetailsActivity extends AppCompatActivity {
     @BindView(R.id.toolbar)
     Toolbar mToolbar;
 
+    @BindView(R.id.collapsingToolbarLayout)
+    CollapsingToolbarLayout collapsingToolbarLayout;
+
     private ProductRepositoryInterface mProductRepository = ProductRepository.getInstance();
 
     @Override
@@ -35,8 +40,18 @@ public class ProductDetailsActivity extends AppCompatActivity {
         Bundle bundle = getIntent().getExtras();
         int productId = bundle.getInt(INTENT_PRODUCT_ID);
         Log.d(getClass().getSimpleName(), "Product id: " + productId);
-        //setupToolBar();
-        //displayData(productId);
+        setupToolBar();
+        displayData(productId);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void displayData(int productId) {
@@ -48,5 +63,7 @@ public class ProductDetailsActivity extends AppCompatActivity {
         setSupportActionBar(mToolbar);
         mToolbar.setTitle("Nieruchomość");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        collapsingToolbarLayout.setTitle("Nieruchomość");
     }
 }
