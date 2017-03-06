1054685b33933b9ddc398b1c64c921d5a3a88125
djawny <djawny@op.pl>
2017-03-06 18:34:23 +0100

    zm

--- app/src/main/java/com/sdaacademy/jawny/daniel/agencjanieruchomosci/database/DatabaseImpl.java
+++ app/src/main/java/com/sdaacademy/jawny/daniel/agencjanieruchomosci/database/DatabaseImpl.java
@@ -11,7 +11,7 @@ public class DatabaseImpl extends SQLiteOpenHelper implements Database {
 
     private static final String DB_CREATE_TODO_TABLE =
             "CREATE TABLE products(" +
-                    "id INTEGER PRIMARY KEY," +
+                    "id INTEGER PRIMARY KEY AUTOINCREMENT," +
                     "name TEXT NOT NULL," +
                     "price INTEGER DEFAULT 0," +
                     "imageName TEXT" +
