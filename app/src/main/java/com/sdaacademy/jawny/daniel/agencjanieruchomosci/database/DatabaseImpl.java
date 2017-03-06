4c5ace3c0c84f01271d105dc5bf0fc30b36db327
djawny <djawny@op.pl>
2017-03-06 20:04:28 +0100

    zm

--- app/src/main/java/com/sdaacademy/jawny/daniel/agencjanieruchomosci/database/DatabaseImpl.java
+++ app/src/main/java/com/sdaacademy/jawny/daniel/agencjanieruchomosci/database/DatabaseImpl.java
@@ -1,13 +1,26 @@
 package com.sdaacademy.jawny.daniel.agencjanieruchomosci.database;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import com.sdaacademy.jawny.daniel.agencjanieruchomosci.model.Product;
+
+import java.util.List;
 
 public class DatabaseImpl extends SQLiteOpenHelper implements Database {
 
-    private final static String NAME = "database.db";
-    private final static int VERSION = 1;
+    private static final String ID = "id";
+    private static final String NAME = "name";
+    private static final String PRICE = "price";
+    private static final String IMAGE_NAME = "imageName";
+    private static final String PRODUCTS = "products";
+    private static final String DATABASE = "database";
+
+    private final static String DB_NAME = "database.db";
+    private final static int DB_VERSION = 1;
 
     private static final String DB_CREATE_TODO_TABLE =
             "CREATE TABLE products(" +
@@ -19,13 +32,12 @@ public class DatabaseImpl extends SQLiteOpenHelper implements Database {
 
     private static final String DROP_TODO_TABLE =
             "DROP TABLE IF EXISTS products";
-
     private static final String ADD_COLUMN =
             "ALTER TABLE products"
                     + "ADD test TEXT";
 
     public DatabaseImpl(Context context) {
-        super(context, NAME, null, VERSION);
+        super(context, DB_NAME, null, DB_VERSION);
     }
 
     @Override
@@ -38,5 +50,19 @@ public class DatabaseImpl extends SQLiteOpenHelper implements Database {
 //        db.execSQL(DROP_TODO_TABLE);
 //        onCreate(db);
 //        db.execSQL(ADD_COLUMN);
+    }
+
+    @Override
+    public void saveProducts(List<Product> products) {
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        for (Product product : products) {
+            contentValues.put(ID, product.getmId());
+            contentValues.put(NAME, product.getmName());
+            contentValues.put(PRICE, product.getmPrice());
+            contentValues.put(IMAGE_NAME, product.getmImageName());
+        }
+        long id = db.insertOrThrow(PRODUCTS, null, contentValues);
+        Log.i(DATABASE, "" + id);
     }
 }
