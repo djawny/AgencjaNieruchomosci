a184026d10a3fc12aca06d3e31111eedde6002d6
djawny <djawny@op.pl>
2017-03-06 18:31:23 +0100

    zm

--- app/src/main/java/com/sdaacademy/jawny/daniel/agencjanieruchomosci/database/DatabaseImpl.java
+++ app/src/main/java/com/sdaacademy/jawny/daniel/agencjanieruchomosci/database/DatabaseImpl.java
@@ -9,6 +9,14 @@ public class DatabaseImpl extends SQLiteOpenHelper implements Database {
     private final static String NAME = "database.db";
     private final static int VERSION = 1;
 
+    private static final String DB_CREATE_TODO_TABLE =
+            "CREATE TABLE products(" +
+                    "id INTEGER PRIMARY KEY," +
+                    "name TEXT NOT NULL," +
+                    "price INTEGER DEFAULT 0," +
+                    "imageName TEXT" +
+                    ");";
+
 
     public DatabaseImpl(Context context) {
         super(context, NAME, null, VERSION);
@@ -16,7 +24,7 @@ public class DatabaseImpl extends SQLiteOpenHelper implements Database {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL(DB_CREATE_TODO_TABLE);
     }
 
     @Override
