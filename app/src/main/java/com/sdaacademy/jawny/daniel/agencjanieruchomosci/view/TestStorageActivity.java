2472dd32317ddef3555c08221dcabca376acc25a
djawny <djawny@op.pl>
2017-03-11 11:12:26 +0100

    zm

--- app/src/main/java/com/sdaacademy/jawny/daniel/agencjanieruchomosci/view/TestStorageActivity.java
+++ app/src/main/java/com/sdaacademy/jawny/daniel/agencjanieruchomosci/view/TestStorageActivity.java
@@ -1,14 +1,18 @@
 package com.sdaacademy.jawny.daniel.agencjanieruchomosci.view;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.sdaacademy.jawny.daniel.agencjanieruchomosci.R;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -27,6 +31,9 @@ public class TestStorageActivity extends AppCompatActivity {
     @BindView(R.id.file)
     TextView mFile;
 
+    @BindView(R.id.image_from_file)
+    ImageView mImage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,6 +45,31 @@ public class TestStorageActivity extends AppCompatActivity {
         String fileName = "myfile";
         writeFile(fileName);
         readFile(fileName);
+        fileName = "myImage";
+        saveImage(fileName);
+        openImage(fileName);
+    }
+
+    private void openImage(String fileName) {
+        try {
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+            Bitmap bitmap = BitmapFactory.decodeStream(openFileInput(fileName), null, options);
+            mImage.setImageBitmap(bitmap);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void saveImage(String fileName) {
+        FileOutputStream outputStream;
+        try {
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.d1);
+            outputStream = openFileOutput(fileName, Context.MODE_PRIVATE);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     private void readFile(String fileName) {
