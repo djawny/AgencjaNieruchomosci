9266a1cfcc7cfca8769b5471f95c8bfa7575c45a
djawny <djawny@op.pl>
2017-03-11 10:17:26 +0100

    zm

--- app/src/main/java/com/sdaacademy/jawny/daniel/agencjanieruchomosci/view/TestStorageActivity.java
+++ app/src/main/java/com/sdaacademy/jawny/daniel/agencjanieruchomosci/view/TestStorageActivity.java
@@ -2,12 +2,20 @@ package com.sdaacademy.jawny.daniel.agencjanieruchomosci.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.sdaacademy.jawny.daniel.agencjanieruchomosci.R;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class TestStorageActivity extends AppCompatActivity {
+
+    @BindView(R.id.text1)
+    TextView mText1;
+
+    @BindView(R.id.text2)
+    TextView mText2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +23,7 @@ public class TestStorageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_test_storage);
         ButterKnife.bind(this);
 
+        mText1.setText(getFilesDir().toString());
+        mText2.setText(getCacheDir().toString());
     }
 }
