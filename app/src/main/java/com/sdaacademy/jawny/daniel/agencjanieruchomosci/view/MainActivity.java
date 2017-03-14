5b74c2392bdd4e06b6e9874c77d07bcf112664a8
djawny <djawny@op.pl>
2017-03-14 20:21:02 +0100

    zm

--- app/src/main/java/com/sdaacademy/jawny/daniel/agencjanieruchomosci/view/MainActivity.java
+++ app/src/main/java/com/sdaacademy/jawny/daniel/agencjanieruchomosci/view/MainActivity.java
@@ -3,7 +3,6 @@ package com.sdaacademy.jawny.daniel.agencjanieruchomosci.view;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
