a978eaecb8dbb730872cfd96be901caf844fadab
djawny <djawny@op.pl>
2017-03-15 12:52:54 +0100

    fragments

---
+++ app/src/main/java/com/sdaacademy/jawny/daniel/agencjanieruchomosci/view/widget/FragmentProductsList.java
@@ -0,0 +1,23 @@
+package com.sdaacademy.jawny.daniel.agencjanieruchomosci.view.widget;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.sdaacademy.jawny.daniel.agencjanieruchomosci.R;
+
+import butterknife.ButterKnife;
+
+public class FragmentProductsList extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.fragment_products_list, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+}
