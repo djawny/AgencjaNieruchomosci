78ddff49717bc5b23df5cde469ceed97aa6193d3
djawny <djawny@op.pl>
2017-03-26 14:18:16 +0200

    fragments

--- app/src/main/java/com/sdaacademy/jawny/daniel/agencjanieruchomosci/view/ProductDetailsActivity.java
+++ app/src/main/java/com/sdaacademy/jawny/daniel/agencjanieruchomosci/view/ProductDetailsActivity.java
@@ -1,42 +1,15 @@
 package com.sdaacademy.jawny.daniel.agencjanieruchomosci.view;
 
-import android.graphics.Color;
 import android.os.Bundle;
-import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.sdaacademy.jawny.daniel.agencjanieruchomosci.R;
-import com.sdaacademy.jawny.daniel.agencjanieruchomosci.model.Product;
-import com.sdaacademy.jawny.daniel.agencjanieruchomosci.repository.ProductRepository;
-import com.sdaacademy.jawny.daniel.agencjanieruchomosci.repository.ProductRepositoryInterface;
 
-import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class ProductDetailsActivity extends AppCompatActivity {
 
-    public static final String INTENT_PRODUCT_ID = ProductDetailsActivity.class.getSimpleName() + "productId";
-
-    @BindView(R.id.product_image)
-    ImageView mProductImage;
-
-    @BindView(R.id.toolbar)
-    Toolbar mToolbar;
-
-    @BindView(R.id.collapsingToolbarLayout)
-    CollapsingToolbarLayout collapsingToolbarLayout;
-
-    @BindView(R.id.product_name)
-    TextView mProductName;
-
-    @BindView(R.id.product_price)
-    TextView mProductPrice;
-
-    private ProductRepositoryInterface mProductRepository = ProductRepository.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,35 +17,8 @@ public class ProductDetailsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_product_details);
         ButterKnife.bind(this);
 
-        Bundle bundle = getIntent().getExtras();
-        int productId = bundle.getInt(INTENT_PRODUCT_ID);
-        Product product = mProductRepository.getProduct(productId);
-        setDisplay(product);
-        setToolBar(product);
+
     }
 
-    private void setDisplay(Product product) {
-        int drawableResourceId = this.getResources().getIdentifier(product.getmImageName(), "drawable", getPackageName());
-        mProductImage.setImageResource(drawableResourceId);
-        mProductName.setText(product.getmName());
-        mProductPrice.setText(String.valueOf(product.getmPrice()));
-    }
 
-    private void setToolBar(Product product) {
-        setSupportActionBar(mToolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        collapsingToolbarLayout.setTitle(product.getmName());
-        collapsingToolbarLayout.setCollapsedTitleTextColor(Color.WHITE);
-        collapsingToolbarLayout.setExpandedTitleColor(Color.BLACK);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                onBackPressed();
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
--- app/src/main/java/com/sdaacademy/jawny/daniel/agencjanieruchomosci/view/widget/FragmentProductDetails.java
+++ app/src/main/java/com/sdaacademy/jawny/daniel/agencjanieruchomosci/view/widget/FragmentProductDetails.java
@@ -1,17 +1,48 @@
 package com.sdaacademy.jawny.daniel.agencjanieruchomosci.view.widget;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.sdaacademy.jawny.daniel.agencjanieruchomosci.R;
+import com.sdaacademy.jawny.daniel.agencjanieruchomosci.model.Product;
+import com.sdaacademy.jawny.daniel.agencjanieruchomosci.repository.ProductRepository;
+import com.sdaacademy.jawny.daniel.agencjanieruchomosci.repository.ProductRepositoryInterface;
+import com.sdaacademy.jawny.daniel.agencjanieruchomosci.view.ProductDetailsActivity;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class FragmentProductDetails extends Fragment {
+
+    public static final String INTENT_PRODUCT_ID = ProductDetailsActivity.class.getSimpleName() + "productId";
+
+    @BindView(R.id.product_image)
+    ImageView mProductImage;
+
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+
+    @BindView(R.id.collapsingToolbarLayout)
+    CollapsingToolbarLayout collapsingToolbarLayout;
+
+    @BindView(R.id.product_name)
+    TextView mProductName;
+
+    @BindView(R.id.product_price)
+    TextView mProductPrice;
+
+    private ProductRepositoryInterface mProductRepository = ProductRepository.getInstance();
 
     @Nullable
     @Override
@@ -19,5 +50,40 @@ public class FragmentProductDetails extends Fragment {
         final View view = inflater.inflate(R.layout.fragment_product_details, container, false);
         ButterKnife.bind(this, view);
         return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Bundle bundle = getActivity().getIntent().getExtras();
+        int productId = bundle.getInt(INTENT_PRODUCT_ID);
+        Product product = mProductRepository.getProduct(productId);
+        setDisplay(product);
+        setToolBar(product);
+    }
+
+    private void setDisplay(Product product) {
+        int drawableResourceId = this.getResources().getIdentifier(product.getmImageName(), "drawable", getActivity().getPackageName());
+        mProductImage.setImageResource(drawableResourceId);
+        mProductName.setText(product.getmName());
+        mProductPrice.setText(String.valueOf(product.getmPrice()));
+    }
+
+    private void setToolBar(Product product) {
+        ((AppCompatActivity) getActivity()).setSupportActionBar(mToolbar);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        collapsingToolbarLayout.setTitle(product.getmName());
+        collapsingToolbarLayout.setCollapsedTitleTextColor(Color.WHITE);
+        collapsingToolbarLayout.setExpandedTitleColor(Color.BLACK);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                getActivity().onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
