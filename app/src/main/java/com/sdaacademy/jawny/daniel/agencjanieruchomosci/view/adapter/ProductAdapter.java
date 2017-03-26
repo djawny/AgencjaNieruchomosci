15df96f983c2f14d22708c4d7c235a0d2b2bd20a
djawny <djawny@op.pl>
2017-03-26 17:17:06 +0200

    listener end

--- app/src/main/java/com/sdaacademy/jawny/daniel/agencjanieruchomosci/view/adapter/ProductAdapter.java
+++ app/src/main/java/com/sdaacademy/jawny/daniel/agencjanieruchomosci/view/adapter/ProductAdapter.java
@@ -2,7 +2,6 @@ package com.sdaacademy.jawny.daniel.agencjanieruchomosci.view.adapter;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -10,13 +9,12 @@ import com.sdaacademy.jawny.daniel.agencjanieruchomosci.R;
 import com.sdaacademy.jawny.daniel.agencjanieruchomosci.model.Product;
 import com.sdaacademy.jawny.daniel.agencjanieruchomosci.view.widget.ProductCardView;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductViewHolder> {
+public class ProductAdapter extends BaseAdapter<Product> {
 
     public interface OnProductSelectedListener {
         void onProductSelected(Product product);
@@ -24,49 +22,26 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductV
 
     private OnProductSelectedListener mListener;
 
-    private List<Product> mProducts;
-    private LayoutInflater mLayoutInflater;
-
     public ProductAdapter(Context context, List<Product> products, OnProductSelectedListener onProductSelectedListener) {
-        mLayoutInflater = LayoutInflater.from(context);
-        mProducts = new ArrayList<>();
-        mProducts.addAll(products);
+        super(context,products);
         mListener = onProductSelectedListener;
     }
 
     @Override
     public ProductViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View itemView = mLayoutInflater.inflate(R.layout.row_layout, parent, false);
+        View itemView = getLayoutInflater().inflate(R.layout.row_layout, parent, false);
         return new ProductViewHolder(itemView);
     }
 
     @Override
-    public void onBindViewHolder(ProductViewHolder holder, int position) {
-        final Product product = getItem(position);
-        holder.bind(product);
+    public void onBind(RecyclerView.ViewHolder holder, final Product product, int position) {
+        ((ProductViewHolder)holder).bind(product);
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mListener.onProductSelected(product);
             }
         });
-    }
-
-    public void swapData(List<Product> products) {
-        if (products != null) {
-            mProducts.clear();
-            mProducts.addAll(products);
-            notifyDataSetChanged();
-        }
-    }
-
-    @Override
-    public int getItemCount() {
-        return mProducts.size();
-    }
-
-    public Product getItem(int position) {
-        return mProducts.get(position);
     }
 
     public static class ProductViewHolder extends RecyclerView.ViewHolder {
