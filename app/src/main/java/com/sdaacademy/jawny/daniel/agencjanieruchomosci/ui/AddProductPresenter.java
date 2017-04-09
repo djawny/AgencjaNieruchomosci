eb01173f00bb2285a6dc9598793a3013d1ae8a78
djawny <djawny@op.pl>
2017-04-09 23:47:18 +0200

    mvp

--- app/src/main/java/com/sdaacademy/jawny/daniel/agencjanieruchomosci/ui/AddProductPresenter.java
+++ app/src/main/java/com/sdaacademy/jawny/daniel/agencjanieruchomosci/ui/AddProductPresenter.java
@@ -1,7 +1,17 @@
 package com.sdaacademy.jawny.daniel.agencjanieruchomosci.ui;
 
 import com.sdaacademy.jawny.daniel.agencjanieruchomosci.base.BasePresenter;
+import com.sdaacademy.jawny.daniel.agencjanieruchomosci.repository.ProductRepositoryInterface;
+import com.sdaacademy.jawny.daniel.agencjanieruchomosci.util.Precondition;
 
-public class AddProductPresenter extends BasePresenter<AddProductView>{
+import io.reactivex.Scheduler;
 
+public class AddProductPresenter extends BasePresenter<AddProductView> {
+
+    private ProductRepositoryInterface mProductRepository;
+
+    public AddProductPresenter(ProductRepositoryInterface productRepository, Scheduler subscriber, Scheduler observer) {
+        super(subscriber, observer);
+        mProductRepository = Precondition.checkNotNull(productRepository);
+    }
 }
--- app/src/main/java/com/sdaacademy/jawny/daniel/agencjanieruchomosci/ui/ProductDetailsPresenter.java
+++ app/src/main/java/com/sdaacademy/jawny/daniel/agencjanieruchomosci/ui/ProductDetailsPresenter.java
@@ -1,7 +1,21 @@
 package com.sdaacademy.jawny.daniel.agencjanieruchomosci.ui;
 
 import com.sdaacademy.jawny.daniel.agencjanieruchomosci.base.BasePresenter;
+import com.sdaacademy.jawny.daniel.agencjanieruchomosci.repository.ProductRepositoryInterface;
+import com.sdaacademy.jawny.daniel.agencjanieruchomosci.util.Precondition;
+
+import io.reactivex.Scheduler;
 
 public class ProductDetailsPresenter extends BasePresenter<ProductDetailsView>{
 
+    private ProductRepositoryInterface mProductRepository;
+
+    public ProductDetailsPresenter(ProductRepositoryInterface productRepository,Scheduler subscriber, Scheduler observer) {
+        super(subscriber, observer);
+        mProductRepository = Precondition.checkNotNull(productRepository);
+    }
+
+    public void loadProduct(){
+
+    }
 }
